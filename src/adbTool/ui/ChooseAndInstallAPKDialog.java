f26911db5390450b4254a4c2220d233bbc0522e7
itzik <itzik.alkotzer@gmail.com>
2014-09-17 18:51:51 +0300

    dialogs - enter for ok esc to cancel

--- src/adbTool/ui/ChooseAndInstallAPKDialog.java
+++ src/adbTool/ui/ChooseAndInstallAPKDialog.java
@@ -5,14 +5,19 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
@@ -25,7 +30,13 @@ public class ChooseAndInstallAPKDialog extends javax.swing.JDialog
     private JTextField _filename;
     private JButton _install;
     private JLabel _title;
+    ActionListener escListener = new ActionListener() {
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+        }
+    };
     public ChooseAndInstallAPKDialog(Frame parent, boolean modal)
     {
         super(parent, modal);
@@ -41,7 +52,10 @@ public class ChooseAndInstallAPKDialog extends javax.swing.JDialog
         initCancelButton();
         initInstallButton();
         initMainPanelAndLayout();
-
+        getRootPane().setDefaultButton(_install);
+        getRootPane().registerKeyboardAction(escListener,
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW);
         pack();
     }
 
@@ -89,7 +103,13 @@ public class ChooseAndInstallAPKDialog extends javax.swing.JDialog
     {
         _install = new JButton();
         _install.setText("Install");
-        _install.addActionListener(actionEvent ->AdbWrapper.getInstance().installApk(_filename.getText()));
+        _install.addActionListener(actionEvent ->
+        {
+            if(!_filename.getText().isEmpty())
+            {
+                AdbWrapper.getInstance().installApk(_filename.getText());
+            }
+        });
     }
 
     private void initBrowseButton()
--- src/adbTool/ui/SendTextDialog.java
+++ src/adbTool/ui/SendTextDialog.java
@@ -1,12 +1,17 @@
 package adbTool.ui;
 
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 
 import adbTool.core.AdbWrapper;
 
@@ -18,11 +23,21 @@ public class SendTextDialog extends JDialog
     private JLabel _title;
     private JPanel jPanel1;
 
+    ActionListener escListener = new ActionListener() {
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+        }
+    };
     public SendTextDialog(Frame parent, boolean modal)
     {
         super(parent, modal);
         initComponents();
+        getRootPane().setDefaultButton(_sendTextButton);
+        getRootPane().registerKeyboardAction(escListener,
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW);
         setActions();
     }
 
